d7c7b0f34a979daf4ad683b047c10c49635b9797
Damian Barrous-Dume <damianbarrous@gmail.com>
2023-02-06 12:46:30 -0500

    Fix typing

--- s3watcher/SQSQueueHandler.py
+++ s3watcher/SQSQueueHandler.py
@@ -125,7 +125,7 @@ class SQSQueueHandler:
         Function to queue messages.
         """
         # Initialize SQSHandlerEvent objects
-        sqs_events = [SQSHandlerEvent(self.sqs, message) for message in messages]
+        sqs_events = [SQSHandlerEvent(self.sqs, message, self.queue_url) for message in messages]
 
         # Concatenate message batch to event array if events don't already exist in it
         for event in sqs_events:
