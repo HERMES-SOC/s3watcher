c7145b6025136f92bc7d38bdc0bf874f48141ba4
Damian Barrous-Dume <damianbarrous@gmail.com>
2023-02-09 11:23:35 -0500

    Check event message

--- s3watcher/SQSHandlerEvent.py
+++ s3watcher/SQSHandlerEvent.py
@@ -8,7 +8,7 @@ class SQSHandlerEvent:
         """
         Class Constructor
         """
-
+        print(sqs_message)
         self.message_id = sqs_message.get("MessageId")
         self.receipt_handle = sqs_message.get("ReceiptHandle")
         message_body = json.loads(sqs_message.get("Body"))
