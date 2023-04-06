37fececcb3b8cb2f704de3a040338325d83ed6c5
Damian Barrous-Dume <damianbarrous@gmail.com>
2023-04-06 18:41:29 +0100

    Refactor to use aws_sdc_utils

--- s3watcher/SQSHandlerEvent.py
+++ s3watcher/SQSHandlerEvent.py
@@ -1,12 +1,12 @@
 import json
 from typing import Any
-from s3watcher import log
+from sdc_aws_utils.logging import log
 
 
 class SQSHandlerEvent:
     def __init__(self, sqs_client: Any, sqs_message: dict, queue_url: str) -> None:
         """
-        Class Constructor
+        Initialize an SQSHandlerEvent instance.
         """
         self.message_id = sqs_message.get("MessageId")
         self.receipt_handle = sqs_message.get("ReceiptHandle")
@@ -30,7 +30,9 @@ class SQSHandlerEvent:
             return False
 
     def get_file_key(self, sqs_client: Any, message_body: str) -> str:
-
+        """
+        Retrieve the S3 Object Key from the message body.
+        """
         # Parse S3 Object Key from Body
         try:
             file_key = message_body.get("Records")[0].get("s3").get("object").get("key")
@@ -47,7 +49,9 @@ class SQSHandlerEvent:
         return file_key
 
     def get_event_type(self, sqs_client: Any, message_body: str) -> str:
-
+        """
+        Retrieve the S3 Event Type from the message body.
+        """
         # Parse S3 Event Type from Body
         try:
             event_type = message_body.get("Records")[0].get("eventName")
@@ -61,7 +65,7 @@ class SQSHandlerEvent:
                 "Error Parsing S3 Event Type from SQS Message Body - Removed Event"
             )
 
-        # Check if it is a Object Created/Updated/Deleted Event
+        # Check if it is an Object Created/Updated/Deleted Event
         if "ObjectCreated" in event_type:
             return "CREATE"
         elif "ObjectRemoved" in event_type:
@@ -73,9 +77,10 @@ class SQSHandlerEvent:
             )
 
     def delete_message(self, sqs_client: Any):
-
+        """
+        Delete the received message from the queue.
+        """
         try:
-
             # Delete received message from queue
             response = sqs_client.delete_message(
                 QueueUrl=self.queue_url, ReceiptHandle=self.receipt_handle
--- s3watcher/__main__.py
+++ s3watcher/__main__.py
@@ -1,27 +1,27 @@
 """
-Main File for the AWS File System Watcher
+AWS File System Watcher Main File
 """
 
 from s3watcher.SQSQueueHandler import SQSQueueHandler
 from s3watcher.SQSQueueHandlerConfig import get_config
 
-# Main Function
+
 def main() -> None:
     """
-    Main Function
-    """
+    Main function that initializes and starts the SQS Queue Handler.
 
-    # Get the Configuration
+    Retrieves the necessary configuration for the SQS Queue Handler and
+    starts the queue handler to process incoming messages.
+    """
+    # Retrieve the configuration
     config = get_config()
 
-    queue_handler = SQSQueueHandler(
-        config=config,
-    )
+    # Initialize the SQS Queue Handler with the given configuration
+    queue_handler = SQSQueueHandler(config=config)
 
-    # Start the Queue Handler
+    # Start the Queue Handler to process incoming messages
     queue_handler.start()
 
 
-# Main Function
 if __name__ == "__main__":
     main()
